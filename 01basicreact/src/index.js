b968b7cff57c5c4c00c97fec0907b01f00aa7e5d
SOWHOM GHOSH <ghoshsowhom638@gmail.com>
2023-11-21 01:13:53 +0530

    third commit

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-
+const anotherElement = 'Here is another element';
+const reactElement = React.createElement(
+  'a',
+  {href: 'http://google.com', target: '_blank'},
+  anotherElement
+  )
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  reactElement
 );
 
