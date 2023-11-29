21824255bfea58f06f42bd6bfbc28d1e5be0284e
SOWHOM GHOSH <ghoshsowhom638@gmail.com>
2023-11-30 02:18:48 +0530

    Email and Password Generator implemented

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef } from 'react'
+import { useState, useCallback, useEffect} from 'react'
 
 
 
@@ -6,53 +6,58 @@ function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [email, setemail] = useState("")
+  const [passWord, setpassWord] = useState("")
 
-  //useRef hook
-  const emailRef = useRef(null)
+ 
 
   const emailGenerator = useCallback(() => {
-    let pass = ""
+    let mail = ""
+    let halfMail = ""
+    let password = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789"
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass += str.charAt(char)
-      
+      mail += str.charAt(char)
     }
-
-    setemail(pass)
-
+    for (let i = 1; i <= length/2; i++) {
+      halfMail += mail.substring(i/2, i)
+      }
+      if(length == 10) password += halfMail.substring(6,  halfMail.length)
+    else if(10>length && length>7) password +=  halfMail.substring(3,  halfMail.length)
+    else password += halfMail.substring(1,  halfMail.length)
+ 
+    setemail(mail)
+    setpassWord(password)
 
   }, [length, numberAllowed, setemail])
-
-  const copyemailToClipboard = useCallback(() => {
-    emailRef.current?.select();
-    emailRef.current?.setSelectionRange(0, 20);
-    window.navigator.clipboard.writeText(email)
-  }, [email])
 
   useEffect(() => {
     emailGenerator()
   }, [length, numberAllowed, emailGenerator])
   return (
     
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 absolute item-center justify-center">
       <h1 className='text-white text-center my-3'>Email generator</h1><br/>
-      <h2 className='text-white text-center my-3'>choose the before part of 'gmail.com' as password</h2>
+      <div>
+      <h2 className='text-white text-center mb-2'>Generated Mail</h2>
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
             type="text"
             value={email+"@gmail.com"}
-            className="outline-none w-full py-1 px-3"
+            className="border-white border-2 w-full py-1 px-3 rounded-2xl"
             placeholder="email"
             readOnly
-            ref={emailRef}
-        />
-        <button
-        onClick={copyemailToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-        >copy</button>
+        /></div><br/>
+      <div> <h2 className='text-white text-center mb-2'>Generated password</h2>
+        <input
+            type="text"
+            value={passWord+"$$"}
+            className="border-white border-2 w-full py-1 px-3 rounded-2xl mb-3"
+            placeholder="password"
+            readOnly
+        /></div> 
         
     </div>
     <div className='flex text-sm gap-x-2'>
@@ -65,7 +70,7 @@ function App() {
          className='cursor-pointer'
          onChange={(e) => {setLength(e.target.value)}}
           />
-          <label>Length: {length}</label>
+          <label className='mx-2'>Length: {length}</label>
       </div>
       <div className="flex items-center gap-x-1">
       <input
