630c9ee448102b25ece60fa4d309bd4d67c18909
SOWHOM GHOSH <ghoshsowhom638@gmail.com>
2023-12-14 23:56:03 +0530

    LocalStorage Context API

--- 06currencyconverter/src/Hooks/useCurrency.js
+++ 06currencyconverter/src/Hooks/useCurrency.js
@@ -6,7 +6,7 @@ const useCurrency = (currency)=>{
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
-    } , [])
+    } , [currency])
         return Data;
 }
 export default useCurrency
