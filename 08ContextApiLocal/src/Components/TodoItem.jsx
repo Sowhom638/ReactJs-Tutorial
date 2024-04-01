708b0212b74e27945d32b2bd66fe27f232e0105b
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2024-04-01 15:02:03 +0530

    let's commit

--- 08ContextApiLocal/src/Components/TodoItem.jsx
+++ 08ContextApiLocal/src/Components/TodoItem.jsx
@@ -38,7 +38,7 @@ function TodoItem({ todo }) {
           />
           {/* Edit, Save Button */}
           <button
-              className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+              className="inline-flex items-center justify-center w-8 h-8 text-sm border rounded-lg border-black/10 bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
               onClick={() => {
                   if (todo.completed) return;
 
@@ -52,7 +52,7 @@ function TodoItem({ todo }) {
           </button>
           {/* Delete Todo Button */}
           <button
-              className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+              className="inline-flex items-center justify-center w-8 h-8 text-sm border rounded-lg border-black/10 bg-gray-50 hover:bg-gray-100 shrink-0"
               onClick={() => deleteTodo(todo.id)}
           >
               ❌
